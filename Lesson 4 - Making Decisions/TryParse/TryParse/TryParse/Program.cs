8ea179984876877779bcf62afdeaefea9b03da81
Thomas Dale <thomasdale92@gmail.com>
2020-02-12 20:09:12 +0000

    4.39/if and else if + TryParse

--- Lesson 4 - Making Decisions/TryParse/TryParse/TryParse/Program.cs
+++ Lesson 4 - Making Decisions/TryParse/TryParse/TryParse/Program.cs
@@ -13,24 +13,55 @@ namespace TryParse
             Console.WriteLine("Whats the temperature like this morning?");
 
             string temperature = Console.ReadLine();
-            int numTemp = int.Parse(temperature);
+            int numTemp;
+            int number;
+            bool userEnteredANumber = int.TryParse(temperature, out number);
+
+            if(userEnteredANumber)
+            {
+                numTemp = number;
+            } else
+            {
+                numTemp = 0;
+                Console.WriteLine("Value entered, was no number. 0 set as temperature");
+            }
 
             if(numTemp < 10)
             {
                 Console.WriteLine("Take the coat");
-            }
-
-            if(numTemp == 10)
+            } else if(numTemp == 20)
             {
                 Console.WriteLine("Pants and a pull over should be fine");
-            }
-
-            if(numTemp > 10)
+            } else if(numTemp > 30) 
             {
-                Console.WriteLine("Shorts are enough for today");
+                Console.WriteLine("It's super hot");
+            } else
+            {
+                Console.WriteLine("Shorts are enough for today!");
             }
 
             Console.Read();
+
+            //TryParse - By using the TryParse() method you can convert a string like '128' into a numeric data type like an integer
+            //TryParse - Often gets used when the user submits input
+
+            string numberAsString = "128";
+            int parsedValue;
+
+            bool success = int.TryParse(numberAsString, out parsedValue);
+
+            //The returned boolean is true when parsing was successful
+
+            if(success)
+            {
+                Console.WriteLine("Parsing successful - number is " + parsedValue);
+            }
+            else
+            {
+                Console.WriteLine("Parsing Unsuccessful");
+            }
+
+            //Parsing Fails
 
         }
     }
