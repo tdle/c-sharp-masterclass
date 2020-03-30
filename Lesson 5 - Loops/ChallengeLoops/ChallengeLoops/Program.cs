da52caa93a7b690e99ad5b7df70fa1309b3b42b5
Thomas Dale <thomasdale92@gmail.com>
2020-03-31 00:05:00 +0100

    Completed Lesson 5.56// Loop challenge and Loop Summary

--- Lesson 5 - Loops/ChallengeLoops/ChallengeLoops/Program.cs
+++ Lesson 5 - Loops/ChallengeLoops/ChallengeLoops/Program.cs
@@ -6,7 +6,50 @@ namespace ChallengeLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Loop and ask for student name & score
+            // Do this for 5 students
+            //come out the loop and print average score of class to console
+
+            string score = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+
+            while (score != "-1")
+            {
+                Console.WriteLine("Last number was {0}", currentNumber);
+                Console.WriteLine("Please enter the next score");
+                Console.WriteLine("Current amount of entries {0}", count);
+                Console.WriteLine("Please enter -1 once you are ready to calculate the average");
+
+                score = Console.ReadLine();
+
+                if(score.Equals("-1"))
+                {
+                    Console.WriteLine("------------------------------------------");
+                    double average = (double)total / (double)count;
+
+                    Console.WriteLine("The average score of your students is {0}", average);
+                }
+
+                if (int.TryParse(score, out currentNumber) && currentNumber > 0 && currentNumber < 21)
+                {
+                    total = total + currentNumber;
+                } 
+                else
+                {
+                    if(!(score.Equals("-1")))
+                    {
+                        Console.Write("Please enter a value between 1 and 20! ");
+                    }
+                    continue;
+                }
+
+                count++;
+            }
+
+            Console.Read();
         }
     }
 }
