6727139516512164f8dc8d25cc32e94954fb0a29
Thomas Dale <thomasdale92@gmail.com>
2020-05-20 13:24:04 +0100

    Started creating first new class to call in main method

---
+++ Lesson 6 - OOP/ConsoleApp1/ConsoleApp1/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop
+{
+    //this class is a blueprint for a datatype
+    class Human
+    {
+        //member variable
+        public string firstName;
+
+        //create a method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0}", firstName);
+        }
+
+    }
+}
--- Lesson 6 - OOP/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lesson 6 - OOP/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,26 @@ namespace Oop
     {
         static void Main(string[] args)
         {
+
+            /*A class is a blueprint of an object
+            It has actions/ abilities - so called member functions or methods
+            and it has properities - so called member variables*/
+
+
+            //Create an object of my class
+            //an instance of human
+
+            Human denis = new Human();
+
+            //Access public variable from outside class, and even change it
+            denis.firstName = "Denis";
+
+            //call methods the class
+            denis.IntroduceMyself();
+
+            Console.ReadKey();
             
+
         }
     }
 }
