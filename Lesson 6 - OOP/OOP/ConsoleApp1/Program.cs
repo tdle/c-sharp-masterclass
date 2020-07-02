1dd96118c014e8eff92cd0234063e792bc3fee18
Thomas Dale <thomasdale92@gmail.com>
2020-07-03 00:45:27 +0100

    Completed lesson on constructors

--- Lesson 6 - OOP/OOP/ConsoleApp1/Program.cs
+++ Lesson 6 - OOP/OOP/ConsoleApp1/Program.cs
@@ -12,14 +12,29 @@ namespace Oop
             and it has properities - so called member variables*/
 
             //Challenge Object
-            Human john = new Human("John", "Wick");
-
+            Human john = new Human("John", "Wick", "Brown", 55);
             john.IntroduceMyself();
 
             //second object for challenge
-            Human harry = new Human("Harry", "Potter");
-
+            Human harry = new Human("Harry", "Potter", "Blue", 18);
             harry.IntroduceMyself();
+
+            //Third object using default constructor
+            Human basicHuman = new Human();
+            basicHuman.IntroduceMyself();
+
+            //second object for challenge
+            Human ron = new Human("Ronald", "Weasley", "Blue");
+            harry.IntroduceMyself();
+
+            Human hermione = new Human("Hermione");
+            hermione.IntroduceMyself();
+
+            Human neville = new Human("Neville", "Longbottom");
+            neville.IntroduceMyself();
+
+            Human draco = new Human("Draco", "Malfoy", 19);
+            draco.IntroduceMyself();
 
             Console.ReadKey();
            
