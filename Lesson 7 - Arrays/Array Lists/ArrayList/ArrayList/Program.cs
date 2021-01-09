a6de3b7a9d639f7ef9fde54a2386ac431780f635
Thomas Dale <thomasdale92@gmail.com>
2021-01-09 20:52:47 +0000

    Completed lesson 7.93 - Array Lists

---
+++ Lesson 7 - Arrays/Array Lists/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare an Array List with undefined amount of objects
+            ArrayList myArrayList = new ArrayList();
+            //Declare an Array List with defined amount of objects
+            ArrayList myArrayList2 = new ArrayList(100);
+
+            myArrayList.Add(25);
+            myArrayList.Add("Hello");
+            myArrayList.Add(13.37);
+            myArrayList.Add(13);
+            myArrayList.Add(128);
+            myArrayList.Add(25.3);
+            
+            //Removal of a specific value from the Array List
+            myArrayList.Remove(13);
+
+            //Delete element at a specific postion
+            myArrayList.RemoveAt(0);
+
+            Console.WriteLine(myArrayList.Count);
+
+            double sum = 0;
+
+            foreach(object obj in myArrayList)
+            {
+                if(obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }else if(obj is double)
+                {
+                    sum += (double)obj;
+                } else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson 7 - Arrays/Jagged Array Challenge/JaggedArrayChallenge/JaggedArrayChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JaggedArrayChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a jagged array, which contains 3 "friends arrays" in which two family members should be stored
+            //Introduce family members from different families to each other via console
+
+            string[][] friendsAndFamily = new string[][]
+            {
+                new string[]{"Michael", "Sandy" },
+                new string[]{"Frank", "Claudia" },
+                new string[]{"Andrew", "Michelle" }
+            };
+
+            Console.WriteLine("Hi {0}, I would like to introduce {1} to you. ", friendsAndFamily[0][0], friendsAndFamily[1][0]);
+            Console.WriteLine("Hi {0}, I would like to introduce {1} to you. ", friendsAndFamily[0][1], friendsAndFamily[2][0]);
+            Console.WriteLine("Hi {0}, I would like to introduce {1} to you. ", friendsAndFamily[0][1], friendsAndFamily[2][1]);
+            Console.ReadKey();
+        }
+    }
+}
